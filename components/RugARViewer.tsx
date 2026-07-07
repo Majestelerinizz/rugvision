0ca7907a363db145ac2a53ae551a652fd565af1f
Yusuf KARAGÜZEL <jose.krgzl156@gmail.com>
2026-07-07 16:16:42 +0300

    chore: silence remaining eslint rules in component and prisma mock

--- components/RugARViewer.tsx
+++ components/RugARViewer.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
 
 import React, { useEffect, useRef, useState, useMemo } from "react";
 import Script from "next/script";
